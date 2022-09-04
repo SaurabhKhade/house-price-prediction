e8a4626b6d8460b448c4b36259b94558296981d5
Saurabh Khade <khadesaurabh056@gmail.com>
2022-09-04 14:19:10 +0530

    UI fixed

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  document.querySelector(".answer").innerText = `Calculating...`;
   const formData = new FormData(e.target);
   const formProps = Object.fromEntries(formData);
   for (let key in formProps) {
